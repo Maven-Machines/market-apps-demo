fe3c0f6f52e79d275e3e1c7fedaf1b9ccea503eb
Daniel Gorziglia <daniel@mavenmachines.com>
2022-07-12 17:21:49 -0400

    implemented Camera Demo Page

--- pages/camera.js
+++ pages/camera.js
@@ -1,9 +1,51 @@
-import styles from '../styles/Home.module.scss'
+import { useState, useEffect } from 'react';
+import styles from '../styles/Camera.module.scss'
+import commonStyles from "../styles/Common.module.scss"
 
 export default function Camera() {
+  const [selectedFile, setSelectedFile] = useState();
+  const [preview, setPreview] = useState()
+
+  useEffect(() => {
+    if (!selectedFile) {
+      setPreview(undefined)
+      return
+    }
+
+    const objectUrl = URL.createObjectURL(selectedFile)
+    setPreview(objectUrl)
+
+    // free memory when ever this component is unmounted
+    return () => URL.revokeObjectURL(objectUrl)
+  }, selectedFile)
+
+  function handleFileSelected(event) {
+		setSelectedFile(event.target.files[0]);
+  }
+
   return (
     <main className={styles.main}>
-      Canera
+      <div className={commonStyles.pageTitle}>
+        Camera Demo
+      </div>
+      <div className={styles.uploadContainer}>
+        <label for="cameraFileInput">
+          <span className={commonStyles.actionButton}>Take Picture</span>
+          <input
+            id={"cameraFileInput"}
+            className={styles.cameraInput}
+            type="file"
+            accept="image/*"
+            capture="environment"
+            onChange={handleFileSelected}
+          />
+        </label>
+      </div>
+      {selectedFile && 
+        <div className={styles.previewContainer}>
+          <img src={preview} />
+        </div>
+      }
     </main>
   )
 }
--- pages/pdf.js
+++ pages/pdf.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import styles from '../styles/Pdf.module.scss'
+import commonStyles from "../styles/Common.module.scss"
 import { Document, Page, pdfjs } from 'react-pdf';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/legacy/build/pdf.worker.min.js`;
@@ -9,14 +10,22 @@ const SAMPLE_PDF_URL = "/sample_pay_stub.pdf";
 export default function PDF() {
   const [pageNumber, setPageNumber] = useState(1);
 
+  function renderLoadingPdf() {
+    return (
+      <div className={styles.loadingPdf}>
+        Loading Document...
+      </div>
+    )
+  }
+
   return (
     <main className={styles.main}>
-      <div className={styles.pdfTitle}>
+      <div className={commonStyles.pageTitle}>
         PDF Demo
       </div>
       <div className={styles.pdfContainer}>
-        <Document file={SAMPLE_PDF_URL}>
-          <Page pageNumber={pageNumber} className={styles.pdfPage} width={1200} />
+        <Document file={SAMPLE_PDF_URL} loading={renderLoadingPdf}>
+          <Page pageNumber={pageNumber} width={1200} />
         </Document>
       </div>
       <div className={styles.pdfDescription}>
