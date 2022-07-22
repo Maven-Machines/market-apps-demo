c3dac68dc4efc9b7c474feae9fcd76b4db160516
Daniel Gorziglia <daniel@mavenmachines.com>
2022-07-22 11:59:08 -0400

    Added document scan + picture scan

--- pages/camera.js
+++ pages/camera.js
@@ -1,7 +1,7 @@
-import Head from 'next/head'
-import { useState, useEffect } from 'react';
-import styles from '../styles/Camera.module.scss'
-import commonStyles from "../styles/Common.module.scss"
+import Head from "next/head";
+import { useState, useEffect } from "react";
+import styles from "../styles/Camera.module.scss";
+import commonStyles from "../styles/Common.module.scss";
 
 export default function Camera() {
   const [selectedFile, setSelectedFile] = useState();
@@ -9,19 +9,19 @@ export default function Camera() {
 
   useEffect(() => {
     if (!selectedFile) {
-      setPreview(undefined)
-      return
+      setPreview(undefined);
+      return;
     }
 
-    const objectUrl = URL.createObjectURL(selectedFile)
-    setPreview(objectUrl)
+    const objectUrl = URL.createObjectURL(selectedFile);
+    setPreview(objectUrl);
 
     // free memory when ever this component is unmounted
-    return () => URL.revokeObjectURL(objectUrl)
-  }, [selectedFile])
+    return () => URL.revokeObjectURL(objectUrl);
+  }, [selectedFile]);
 
   function handleFileSelected(event) {
-		setSelectedFile(event.target.files[0]);
+    setSelectedFile(event.target.files[0]);
   }
 
   return (
@@ -29,14 +29,12 @@ export default function Camera() {
       <Head>
         <title>Camera Demo</title>
       </Head>
-      <div className={commonStyles.pageTitle}>
-        Camera Demo
-      </div>
+      <div className={commonStyles.pageTitle}>Camera Demo</div>
       <div className={styles.uploadContainer}>
-        <label htmlFor="cameraFileInput">
+        <label htmlFor="imageFileInput">
           <span className={commonStyles.actionButton}>Take Picture</span>
           <input
-            id={"cameraFileInput"}
+            id={"imageFileInput"}
             className={styles.cameraInput}
             type="file"
             accept="image/*"
@@ -44,15 +42,27 @@ export default function Camera() {
             onChange={handleFileSelected}
           />
         </label>
+        <div className={styles.spacer} />
+        <label htmlFor="scanFileInput">
+          <span className={commonStyles.actionButton}>Take Document Scan</span>
+          <input
+            id={"scanFileInput"}
+            className={styles.cameraInput}
+            type="file"
+            accept="image/*, .mavenScan"
+            capture="environment"
+            onChange={handleFileSelected}
+          />
+        </label>
       </div>
-      {selectedFile && 
+      {selectedFile && (
         <div className={styles.previewContainer}>
           <picture>
             <source srcSet={preview} />
             <img src={preview} alt="Image Preview" />
           </picture>
         </div>
-      }
+      )}
     </main>
-  )
+  );
 }
