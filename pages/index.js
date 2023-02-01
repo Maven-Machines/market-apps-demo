23f1d473127478b36efa9aff74e330abdb1e1fb1
Avi Geller <avishai.geller@gmail.com>
2023-02-01 12:32:18 -0500

    Adding maps

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,8 @@ export default function Home({ driver, deviceLocation }) {
         <h1>Hi {driver.firstName}!</h1>
         <p>Tap the cards below to launch a demo</p>
 		<p>{JSON.stringify(deviceLocation)}</p>
+		<a href="https://goo.gl/maps/Ez6FuYbPg24sjDDx7">URL to Google</a>
+		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1693.6496982439564!2d-79.95507948009957!3d40.46362292252673!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xff43c6cc8d1c972b!2zNDDCsDI3JzQ5LjYiTiA3OcKwNTcnMTMuOCJX!5e1!3m2!1sen!2sus!4v1675272625497!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       </div>
     );
   }
